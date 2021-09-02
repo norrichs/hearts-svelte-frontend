395342e62a01f9ef3d67c5893edbe3d8d3181be0
ben.norrichs@gmail.com <ben.norrichs@gmail.com>
2021-09-02 16:00:51 -0400

    minor updates to clean up for deployment

--- src/App.svelte
+++ src/App.svelte
@@ -2,29 +2,18 @@
 	import { Router, Route, Link } from "svelte-routing";
 	import Lobby from "./pages/Lobby.svelte";
 	import Game from "./pages/Game.svelte";
-	import {} from 'svelte/easing'
+	import {} from "svelte/easing";
 	// import SandBox from "./pages/SandBox.svelte";
 
 	export let url = "";
 	const dbUrl = "http://localhost:4500";
 	const user = 0;
-
 </script>
 
 <Router {url}>
-	<!-- <header>
-		<nav>
-			<div>test</div>
-			<Link to="/">Lobby</Link>
-			<Link to="/game/">Game test</Link>
-			<Link to="/sandbox/">Sandbox</Link>
-		</nav>
-	</header> -->
-	<div>
-		<Route path="/"><Lobby /></Route>
-		<Route path="/game/"><Game {dbUrl} {user} /></Route>
-		<!-- <Route path="/sandbox/"><SandBox /></Route> -->
-	</div>
+	<Route path="/"><Lobby /></Route>
+	<Route path="/game/"><Game {dbUrl} {user} /></Route>
+	<!-- <Route path="/sandbox/"><SandBox /></Route> -->
 </Router>
 
 <style>
@@ -33,25 +22,23 @@
 		--card-ratio: 0.7;
 		--card-height: 150px;
 		--card-width: calc(var(--card-height) * var(--card-ratio));
-		--card-border-radius: calc( var(--card-height) / 15);
+		--card-border-radius: calc(var(--card-height) / 15);
 		/* colors */
 		--cardfront-background: rgb(231, 231, 230);
-		--cardback-background: radial-gradient(circle, rgba(70,163,57,1) 0%, rgba(15,91,17,1) 100%);
+		--cardback-background: radial-gradient(
+			circle,
+			rgba(70, 163, 57, 1) 0%,
+			rgba(15, 91, 17, 1) 100%
+		);
 		--redsuit-color: rgb(143, 2, 2);
 		--blacksuit-color: rgb(0, 10, 3);
 		--select-distance: -0.4;
 		--select-speed: 450ms;
-		--select-scale: 1.0;
+		--select-scale: 1;
 		--select-rotation: 0deg;
 		--header-height: 100px;
 		--card-overlap-reveal: 0.2;
 		--trick-card-scale: 1;
 		--npc-scale: 0.6;
 	}
-	/* header{
-		height: var(--header-height)
-	}
-	nav {
-		border: 1px solid magenta;
-	} */
 </style>
--- src/pages/Lobby.svelte
+++ src/pages/Lobby.svelte
@@ -1,11 +1,51 @@
 <script>
-	import {Link} from 'svelte-routing'
+	import { Link } from "svelte-routing";
 	const currentDate = new Date();
 </script>
 
 <main>
-	<h1>Hearts, by norrichs</h1>
-	<h2>Lobby</h2>
-	<p>{currentDate}</p>
-	<Link to="/game/"><button>Play Game</button></Link>
+	<section>
+		<h1>Hearts, by norrichs</h1>
+		<h2>Lobby</h2>
+		<ul>
+			<li>Under construction.</li>
+			<li>
+				Game is currently configured to run in single-player mode, with 3
+				computer opponents.
+			</li>
+			<li>Opponents play with a 'random valid card' strategy.</li>
+		</ul>
+		<h2>Coming soon...</h2>
+		<ul>
+			<li>Multi-player mode with opponent matching</li>
+			<li>Configurable game parameters (win score, animation speed, etc)</li>
+		</ul>
+		<h2>More info</h2>
+		<ul>
+			<li><a href="https://github.com/norrichs/hearts-game">Backend Github</a></li>
+			<li><a href="https://github.com/norrichs/hearts-svelte-frontend">Frontend Github</a></li>
+			<li><a href="https://norrichs.com">My Portfolio</a></li>
+		</ul>
+		<Link to="/game/"><button>Play Game</button></Link>
+	</section>
 </main>
+
+<style>
+	main{
+		display: grid;
+		place-items: center;
+		background-color: dimgrey;
+		width: 100%;
+		height: 100%;
+	}
+	section{
+		background-color: white;;
+		max-width: 500px;
+		padding: 40px;
+		border-radius: 5px;
+		box-shadow: 0 0 20px 0px black;
+	}
+	button{
+		width: 100%;
+	}
+</style>
